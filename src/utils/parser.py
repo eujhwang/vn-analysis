0a51d421955c9788c7d1736c45fc5387883d2e80
m.smriti.95@gmail.com <smritimurali@Smritis-MacBook-Pro.local>
2021-04-08 04:03:50 -0400

    Update model choices

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -25,7 +25,7 @@ def register_default_args(parser, ident):
     parser.add_argument('--use_gdc', action='store_true',
                         help='Use GDC preprocessing.')
     parser.add_argument('--model', type=str, default="appnp",
-                        choices=["mlp", "gcn", "sage", "mlp", "gat", "sgc", "gin", "gcn-v", "sage-v"])
+                        choices=["mlp", "gcn", "sage", "mlp", "gat", "sgc", "gin", "gcn-v", "sage-v","appnp","gdc"])
     parser.add_argument('--layers', type=int, default=2)
     parser.add_argument('--hid_dim', type=int, default=64)
     parser.add_argument('--dropout', type=float, default=0.0)
