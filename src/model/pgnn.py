3516e29cd3ee6ca1e3a308ea665597e9b60915be
Eunjeong Hwang <hej78520@gmail.com>
2021-05-14 21:29:39 -0400

    pgnn

--- src/model/pgnn.py
+++ src/model/pgnn.py
@@ -273,7 +273,7 @@ class PGNN(torch.nn.Module):
         x = data.x
         if self.feature_pre:
             x = self.linear_pre(x)
-        x_position, x = self.conv_first(x, data.dists_max, data.dists_argmax)
+        x_position, x = self.conv_first(x, data.dists_max, data.dists_argmax).to(data.device)
         if self.layer_num == 1:
             return x_position
         # x = F.relu(x) # Note: optional!
