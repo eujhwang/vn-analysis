3e5b203bcec3381f53ebad991795b76d1b20fff1
eujhwang <hej78520@gmail.com>
2022-08-01 23:19:01 +0900

    default option update

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -19,7 +19,7 @@ def register_default_args(parser, ident):
     parser.add_argument('--eval_steps', type=int, default=1)  # LEAVE 1 for now since logger not adapted for others yet
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--epochs', type=int, default=500 if torch.cuda.is_available() else 30)
-    parser.add_argument('--patience', default=50, type=int)
+    parser.add_argument('--patience', default=30, type=int)
     parser.add_argument('--log_batch_size', type=int, default=14)
     parser.add_argument('--data_dir', type=str, default="data")
     parser.add_argument('--model', type=str, default="gcn",
@@ -28,7 +28,7 @@ def register_default_args(parser, ident):
                                  "gcn-vn", "sage-vn", "gin-vn"])
     parser.add_argument('--clusters', type=int, default=0)  # metis+
     parser.add_argument('--vns', type=int, default=0)
-    parser.add_argument('--vns_conn', type=int, default=3)  # is ignored (always considered 1) with graclus currently
+    parser.add_argument('--vns_conn', type=int, default=1)  # is ignored (always considered 1) with graclus currently
     parser.add_argument('--vn_idx', type=str, default="full", choices=["full", "random", "random-f", "random-e", "graclus", "diffpool", "metis", "metis+"])
     parser.add_argument('--K', type=int, default=10)  # different meanings in different models
     parser.add_argument('--alpha', type=float, default=0.5)
@@ -36,8 +36,8 @@ def register_default_args(parser, ident):
                         help='PGNN: k-hop shortest path distance. -1 means exact shortest path')  # -1, 2
     parser.add_argument('--anchors', type=int, default=64)  # PGNN
 
-    parser.add_argument('--layers', type=int, default=2)
-    parser.add_argument('--hid_dim', type=int, default=64)
+    parser.add_argument('--layers', type=int, default=3)
+    parser.add_argument('--hid_dim', type=int, default=256)
     parser.add_argument('--dropout', type=float, default=0.0)
 
     parser.add_argument('--seed', type=int, default=random.randint(0, 2 ** 32), help="seed for random number generator")
@@ -46,8 +46,8 @@ def register_default_args(parser, ident):
                         help="train_idx files for ogbl-ppa. train50 : 50% of train data")
 
     # virtual nodes
-    parser.add_argument('--JK', type=str, default="last", choices=["last", "sum"], help="how to combine nodes at the end")
-    parser.add_argument('--activation', type=str, default="relu", choices=["relu", "leaky", "elu"],
+    parser.add_argument('--JK', type=str, default="sum", choices=["last", "sum"], help="how to combine nodes at the end")
+    parser.add_argument('--activation', type=str, default="leaky", choices=["relu", "leaky", "elu"],
                         help="activation layer for gnn-v relu: ReLU, leaky: LeakyReLU, elu: ELU")
     parser.add_argument('--aggregation', type=str, default="sum", choices=["sum", "mean", "max"],
                         help="aggregation for virtual nodes")
